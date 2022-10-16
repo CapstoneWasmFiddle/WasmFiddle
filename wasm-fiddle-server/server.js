3378d5f8f415aedf56011df146feb1848c34525f
Benny <96bennyl@gmail.com>
2022-10-16 11:32:05 -0700

    fix: change from require to import, change port

--- wasm-fiddle-server/server.js
+++ wasm-fiddle-server/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
+import express from express
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 app.get('/status', (req, res) => {
     res.sendStatus(200)
